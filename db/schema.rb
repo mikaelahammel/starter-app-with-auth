9c7f23d1df5fd602e896f4e1fec5c017a0f5f752
Mikaela Hammel <mikaela.hammel@kellogg.northwestern.edu>
2023-06-01 16:17:00 +0000

    code along

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_01_151935) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_01_150204) do
+  create_table "posts", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "real_name"
